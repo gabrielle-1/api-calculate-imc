beedd315a448d4a1426d019bd8fcf41eaaf290fc
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-05-08 23:05:55 -0300

    Added a cors

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
 app.use(routes);
+
+app.use(cors({
+    origin: ['http://localhost:21262', 'http://127.0.0.1:5500/Projeto%20GYM/FrontEnd/index.html', 'http://127.0.0.1:5500/Projeto%20GYM/FrontEnd']
+}));
 
 app.listen(21262, () =>{
     console.log('Expressa started em http://localhost:21262')
